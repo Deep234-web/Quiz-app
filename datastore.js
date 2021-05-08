fc62baa0a00d43df404186c95673b18c304ad400
Deep <deeprajani1999@gmail.com>
2021-05-08 14:23:59 +0530

    Initial commit

---
+++ datastore.js
@@ -0,0 +1,105 @@
+//BreakingBad question and answer data sore//
+const STORE = [
+{
+question: "Hyper Text Markup Language Stand For?",
+ans1: 'Javascript',
+ans2: 'XML',
+ans3: 'HTML',
+ans4: 'CSS',
+
+correctAnswer: 'HTML',
+},
+{
+question: 'Chandler is a teacher of what high school subject?',
+ans1: 'Chemistry',
+ans2: 'Biology',
+ans3: 'Physics',
+ans4: 'Geology',
+
+correctAnswer: 'Chemistry',
+},
+{
+question: "Which language is used for styling web pages?",
+ans1: 'HTML',
+ans2: 'CSS',
+ans3: 'Java',
+ans4: 'XML',
+
+correctAnswer: 'CSS',
+},
+{
+question: "Which is not a JavaScript Framework?",
+ans1: 'Javascript',
+ans2: 'ReactJS',
+ans3: 'Python',
+ans4: 'Nodejs',
+
+correctAnswer: 'Python',
+},
+{
+question: "What's the name of the fried chicken restaurant owned by Gus?",
+ans1: 'Pollos',
+ans2: 'Pollo Feliz',
+ans3: 'Delicioso Pollo',
+ans4: 'Amigos de Pollo',
+
+correctAnswer: 'Pollos',
+},
+{
+question: 'Which is used for Connect To Database?',
+ans1: 'HTML',
+ans2: 'CSS',
+ans3: 'XML',
+ans4: 'PHP',
+
+correctAnswer: 'PHP',
+},
+{
+question: 'Webdevtrick.com is about..',
+ans1: 'Web Design',
+ans2: 'Graphic Design',
+ans3: 'SEO',
+ans4: 'ALL',
+
+correctAnswer: 'ALL',
+},
+{
+question: 'What is Python?',
+ans1: 'Snake',
+ans2: 'Bird',
+ans3: 'Programming Language',
+ans4:'Animal',
+correctAnswer: 'Programming Language',
+},
+{
+question: 'What is the best framework?',
+ans1: 'Reactjs',
+ans2: 'Angularjs',
+ans3: 'Vuejs',
+ans4: 'ALL',
+
+correctAnswer: 'ALL',
+},
+{
+question: 'What is the best Language to learn?',
+ans1: 'Python',
+ans2: 'Java',
+ans3: 'C',
+ans4: 'ALL',
+
+correctAnswer: 'ALL',
+}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
